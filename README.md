e139d7187da174e02689b81e70efd317fc1fbefa
Andrew Buttigieg <andrewbuttigieg@gmail.com>
2016-06-29 15:00:23 +0200

    moved from dnx to dotnet toolchain

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # dotnet-unit-convertor
 
-Run dnu restore to build projects.
+Run dotnet restore to build projects.
 
-After this, do cd dotnet_unit_convertor.tests and run dnx test to run all the tests to verfiy everything is ok.
+After this, do cd dotnet_unit_convertor.tests and run dotnet test to run all the tests to verfiy everything is ok.
 
-From here, do cd .. and cd src and run dnx src to run the application.
+From here, do cd .. and cd src and run dotnet run to run the application.
--- dotnet_unit_convertor.tests/ConvertorTests.cs
+++ dotnet_unit_convertor.tests/ConvertorTests.cs
@@ -1,19 +1,29 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Xunit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace dotnet_unit_convertor
 {
+    [TestClass]
     public class ConvertorTests
     {
-        [Fact]
-        public void Convertor_100Fran_38Celcius()
+        [TestMethod]
+        public void Convertor_100Fran_37_78Celcius()
         {
             var convertor = new Convertor();
             var celcius =  convertor.Convert("f", "c", 100);
 
-            Assert.Equal(celcius, 37.78m);
+            Assert.AreEqual(celcius, 37.78m);
+        }
+
+        [TestMethod]
+        public void Convertor_37_78Celcius_100Fran()
+        {
+            var convertor = new Convertor();
+            var celcius =  convertor.Convert("c", "f", 37.78m);
+
+            Assert.AreEqual(celcius, 100m);
         }
     }
 }
