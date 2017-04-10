b66cab0d85776d338ed9d389e46734a2ce31f18d
Andrew Buttigieg <andrewbuttigieg@gmail.com>
2017-04-10 19:44:11 +0200

    Update TupleFactory.cs

--- src/TupleFactory.cs
+++ src/TupleFactory.cs
@@ -4,9 +4,7 @@ namespace dotnet_unit_convertor
 {
     public class TupleFactory{
         public static Tuple<string, string> Gen(string from, string to){
-            return 
-                breaks the build 
-                new Tuple<string, string>(from, to);
+            return new Tuple<string, string>(from, to);
         }
     }
 }
